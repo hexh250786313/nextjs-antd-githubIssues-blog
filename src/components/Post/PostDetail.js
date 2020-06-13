4fafe8f3535b5446899b3cc79f4127a3ae5a1552
howard <250786313@qq.com>
2020-06-13 23:37:23 +0800

    update

--- src/components/Post/PostDetail.js
+++ src/components/Post/PostDetail.js
@@ -1,10 +1,14 @@
 import PropTypes from 'prop-types';
 import ReactMarkdown from 'react-markdown';
 import React from 'react';
+import CodeBlock from '../CodeBlock';
+import './index.less';
+import MarkdownNavbar from 'markdown-navbar';
+import { Anchor } from 'antd';
 
 export default class PostDetail extends React.Component {
   render() {
-    const {
+    let {
       detail: { title = '', body = '', number = '' },
     } = this.props;
     return (
@@ -13,13 +17,21 @@ export default class PostDetail extends React.Component {
           {number}
           {title}
         </p>
-        <ReactMarkdown source={body} escapeHtml={false}></ReactMarkdown>
-        {
-          // <ReactMarkdown
-          // source={detail[0] ? detail[0].body : ''}
-          // escapeHtml={false}
-          // ></ReactMarkdown>
-        }
+        <ReactMarkdown
+          className='markdown-body'
+          source={body}
+          renderers={{
+            code: CodeBlock,
+          }}
+          escapeHtml={false}
+        ></ReactMarkdown>
+        <Anchor style={{ position: `fixed`, top: 80, right: 0 }}>
+          <div className='markNav-title'>文章目录</div>
+
+          <div className='navigation'>
+            <MarkdownNavbar ordered={false} source={body} />
+          </div>
+        </Anchor>
       </div>
     );
   }
