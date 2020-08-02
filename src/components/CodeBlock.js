76df3e6082edf2d8fca22d9b95d2e80da5ee2a60
howard <250786313@qq.com>
2020-08-02 11:28:35 +0800

    update

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -1,13 +1,17 @@
-// import { useEffect } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter'
-// import theme from 'react-syntax-highlighter/dist/cjs/styles/prism/solarizedlight'
-import theme from 'react-syntax-highlighter/dist/cjs/styles/prism/vs-dark'
-// import {
-// jsx,
-// javascript,
-// bash,
-// } from 'react-syntax-highlighter/dist/cjs/languages/prism'
+// 设置高亮样式
+import vsDark from 'react-syntax-highlighter/dist/cjs/styles/prism/vs-dark'
+// 设置高亮的语言
+import {
+  jsx,
+  javascript,
+  bash,
+} from 'react-syntax-highlighter/dist/cjs/languages/prism'
+
+// import jsx from 'react-syntax-highlighter/dist/cjs/languages/prism/jsx';
+// import javascript from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
 
 class CodeBlock extends React.PureComponent {
   static propTypes = {
@@ -19,53 +23,24 @@ class CodeBlock extends React.PureComponent {
     language: null,
   }
 
-  // componentDidMount() {
-  // // 注册要高亮的语法，
-  // // 注意：如果不设置打包后供第三方使用是不起作用的
-  // SyntaxHighlighter.registerLanguage('jsx', jsx)
-  // SyntaxHighlighter.registerLanguage('javascript', javascript)
-  // SyntaxHighlighter.registerLanguage('js', javascript)
-  // SyntaxHighlighter.registerLanguage('shell', bash)
-  // }
+  componentWillMount() {
+    // 注册要高亮的语法，
+    // 注意：如果不设置打包后供第三方使用是不起作用的
+    SyntaxHighlighter.registerLanguage('jsx', jsx)
+    SyntaxHighlighter.registerLanguage('javascript', javascript)
+    SyntaxHighlighter.registerLanguage('js', javascript)
+    SyntaxHighlighter.registerLanguage('shell', bash)
+  }
 
   render() {
     const { language, value } = this.props
     return (
       <figure className="highlight">
-        <SyntaxHighlighter language={language} style={theme}>
+        <SyntaxHighlighter language={language} style={vsDark}>
           {value}
         </SyntaxHighlighter>
       </figure>
     )
   }
 }
-
-// const CodeBlock = ({ value, language }) => {
-// useEffect(() => {
-// // 注册要高亮的语法，
-// // 注意：如果不设置打包后供第三方使用是不起作用的
-// SyntaxHighlighter.registerLanguage('jsx', jsx)
-// SyntaxHighlighter.registerLanguage('javascript', javascript)
-// SyntaxHighlighter.registerLanguage('js', javascript)
-// SyntaxHighlighter.registerLanguage('shell', bash)
-// }, [])
-
-// return (
-// <figure className="highlight">
-// <SyntaxHighlighter language={language} style={theme}>
-// {value}
-// </SyntaxHighlighter>
-// </figure>
-// )
-// }
-
-CodeBlock.defaultProps = {
-  language: null,
-}
-
-CodeBlock.propTypes = {
-  value: PropTypes.string.isRequired,
-  language: PropTypes.string,
-}
-
 export default CodeBlock
