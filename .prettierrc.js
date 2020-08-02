dbfe12b556646ca4adaa8ab963b0d9b37aa7c4bf
howard <250786313@qq.com>
2020-08-02 10:54:53 +0800

    update

--- .prettierrc.js
+++ .prettierrc.js
@@ -5,5 +5,6 @@ module.exports = {
   singleQuote: true,
   // jsxSingleQuote: true,
   trailingComma: 'all',
-};
+  semi: false,
+}
 
--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -1,21 +1,20 @@
-// import { useEffect } from 'react';
+import { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter'
 import theme from 'react-syntax-highlighter/dist/cjs/styles/prism/solarizedlight'
-// import jsx from 'react-syntax-highlighter/dist/cjs/languages/prism/jsx';
-// import javascript from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
-// import bash from 'react-syntax-highlighter/dist/cjs/languages/prism/bash';
+import jsx from 'react-syntax-highlighter/dist/cjs/languages/prism/jsx'
+import javascript from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript'
+import bash from 'react-syntax-highlighter/dist/cjs/languages/prism/bash'
 
 const CodeBlock = ({ value, language }) => {
-  // useEffect(() => {
-  // // 注册要高亮的语法，
-  // // 注意：如果不设置打包后供第三方使用是不起作用的
-  // }, []);
-
-  // SyntaxHighlighter.registerLanguage('jsx', jsx);
-  // SyntaxHighlighter.registerLanguage('javascript', javascript);
-  // SyntaxHighlighter.registerLanguage('js', javascript);
-  // SyntaxHighlighter.registerLanguage('shell', bash);
+  useEffect(() => {
+    // 注册要高亮的语法，
+    // 注意：如果不设置打包后供第三方使用是不起作用的
+    SyntaxHighlighter.registerLanguage('jsx', jsx)
+    SyntaxHighlighter.registerLanguage('javascript', javascript)
+    SyntaxHighlighter.registerLanguage('js', javascript)
+    SyntaxHighlighter.registerLanguage('shell', bash)
+  }, [])
 
   return (
     <figure className="highlight">
