54ac7f959ada13e090b8362100967014cb9698a4
Howard He <250786313@qq.com>
2020-09-21 15:51:27 +0800

    update: add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types'
+import ErrorPage from '@/components/ErrorPage'
+
+const Error = ({ statusCode }) => (
+  <ErrorPage statusCode={statusCode || 200} />
+)
+
+Error.propTypes = {
+  statusCode: PropTypes.number,
+}
+
+Error.defaultProps = {
+  statusCode: 200,
+}
+
+Error.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : null
+  return { statusCode }
+}
+
+export default Error
