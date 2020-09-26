49a8dd7880bf19daa95a2743b3c6f251a628fd81
hexh <250786313@qq.com>
2020-09-26 11:42:03 +0800

    fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -13,9 +13,9 @@ Error.defaultProps = {
   statusCode: 200,
 }
 
-Error.getInitialProps = ({ res, err }) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : null
-  return { statusCode }
-}
+// Error.getInitialProps = ({ res, err }) => {
+//   const statusCode = res ? res.statusCode : err ? err.statusCode : null
+//   return { statusCode }
+// }
 
 export default Error
